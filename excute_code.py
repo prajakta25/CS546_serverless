afb2c38dceb79f5fcf12d3960e2159f5066a0794
prajakta25 <prajaktarajhans96@gmail.com>
2019-11-23 18:37:48 -0600

    changed  excute_code

--- excute_code.py
+++ excute_code.py
@@ -2,16 +2,16 @@ import os
 
 def control():
     '''print("tell me to stop by pressing enter")'''
-    stopping = raw_input("Press enter to stop")
-    with open("stop",'w') as stop_file:
-        stop_file.write("1")
-    os.system(" python printTable.py")
+    stopping = input("Press 1 to stop")
+    if stopping > 0:
+        os.system("pkill -f comm_exec.sh")
+    os.system("python printTable.py")
+       
     
 def execution():
     with open("stop",'w') as stop_file:
         stop_file.write("0")
     os.system("bash comm_exec.sh &")
-    '''os.system(" python printTable.py")'''
 def main():
     
     pid = os.fork()
